4e47ba19ab47919773d21f3e70e9978f55565269
Anuroop P S <anuroopps2001@gmail.com>
2026-01-01 14:10:20 +0530

    Fix incorrect user route definitions for PUT and DELETE endpoints

--- routes.go
+++ routes.go
@@ -3,6 +3,6 @@ package main
 func (s *MuxServer) routes() {
 	s.gorilla.HandleFunc("/user", s.addUser).Methods("POST")
 	s.gorilla.HandleFunc("/users", s.listUsers).Methods("GET")
-	s.gorilla.HandleFunc("/user{id}", s.updateUser).Methods("PUT")
-	s.gorilla.HandleFunc("user{id}", s.deleteUser).Methods("DELETE")
+	s.gorilla.HandleFunc("/user/{id}", s.updateUser).Methods("PUT")
+	s.gorilla.HandleFunc("user/{id}", s.deleteUser).Methods("DELETE")
 }
