cf5a99ac9ffaef80989eb4d2a0ce554cc06719cd
Anuroop P S <anuroopps2001@gmail.com>
2026-01-04 09:25:45 +0530

    added shell script to install and start minikube

---
+++ scripts/go-minikube-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "===== Installation started ====="
+
+echo "===== minikube installation started ===="
+sudo apt update
+sudo apt install -y curl wget apt-transport-https
+
+echo "==== minikube package started downloading ===="
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+echo "==== minikube version ===="
+minikube version
+
+echo "==== minikube installation completed ===="
+
+
+echo "=== kubectl package download started ===="
+curl -LO "https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install kubectl /usr/local/bin/kubectl
+echo "=== kubectl donwload completed"
+
+echo "=== Adding ubuntu user into docker group ==="
+sudo usemod -aG docker ubuntu
+sudo newgrp docker
+
+sudo systemctl restart docker
+
+echo "=== added ubuntu user into docker group ==="
+
+echo "=== start minikube ===="
+minikube start --driver=docker
+echo "=== minikube started successfully...!! ===="
+
+
