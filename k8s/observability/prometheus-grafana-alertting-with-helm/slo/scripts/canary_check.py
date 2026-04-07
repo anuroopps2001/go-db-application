7e5e419c05443b4494ab44a27364bfc7d39cc1df
Anuroop P S <anuroopps2001@gmail.com>
2026-04-07 16:47:07 +0530

    updated python script to use azure managed prometheus

--- k8s/observability/prometheus-grafana-alertting-with-helm/slo/scripts/canary_check.py
+++ k8s/observability/prometheus-grafana-alertting-with-helm/slo/scripts/canary_check.py
@@ -1,17 +1,35 @@
 import requests
 import sys
 import os
+from azure.identity import DefaultAzureCredential
+
 
 TARGET_NAMESPACE = os.getenv("TARGET_NAMESPACE", "go-db-app-dev")
-BASE_URL = os.getenv("PROMETHEUS_URL", "http://localhost:9090").rstrip('/')
+BASE_URL = os.getenv("PROMETHEUS_URL", "https://go-app-prometheus-geacauf4ewaxgrd7.southindia.prometheus.monitor.azure.com").rstrip('/')
 PROMETHEUS_URL = f"{BASE_URL}/api/v1/query"
 CANARY_VERSION= "02"
 ERROR_THRESHOLD=2.0
 LATENCY_THRESHOLD=0.05
 
-def query_prometheus(query):
+def get_azure_token():
+    """Fetches a Bearer token for Azure Managed Prometheus."""
     try:
-        response = requests.get(PROMETHEUS_URL, params={'query': query})
+        # DefaultAzureCredential works in local Dev (CLI login) and AKS (Managed Identity)
+        credential = DefaultAzureCredential()
+        # The scope for Managed Prometheus is fixed
+        token = credential.get_token("https://prometheus.monitor.azure.com/.default")
+        return token.token
+    except Exception as e:
+        print(f"Authentication Error: {e}")
+        sys.exit(1)
+
+def query_prometheus(query):
+    token = get_azure_token()
+    headers = {"Authorization": f"Bearer {token}"}
+
+    try:
+        response = requests.get(PROMETHEUS_URL, params={'query': query}, headers=headers)
+        response.raise_for_status() # Catch 401/403 errors
         data = response.json()
         results = data.get('data', {}).get('result', []) # List of dictionaries
         
